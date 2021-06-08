02f7ea33830954ad576930cf46f4a000d1c8e2a9
Tiziano Radicchi <63679072+tiz314@users.noreply.github.com>
2021-06-08 10:41:21 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:2048
 openssl rsa -pubout -in private_key.pem -out public_key.pem
 touch text.txt
